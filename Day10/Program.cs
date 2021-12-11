c21553f31b241e025d5bd91bbb523d5410e448d1
Zlatko Radojcic <zlatko.rad.alt@gmail.com>
2021-12-11 09:45:36 +0100

    Solutions for previous days

---
+++ Day10/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Day10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Solve1(InputData.GetInput());
+            Solve2(InputData.GetInput());
+        }
+
+        static void Solve1(string[] input)
+        {
+            var errors = input.Select(x => GetFirstIllegalChar(x));
+
+            Console.WriteLine($"Sum of errors is {errors.Sum(x => x.Item1)}");
+        }
+
+        static void Solve2(string[] input)
+        {
+            var notCorrupted = input.Select(x => GetFirstIllegalChar(x)).Where(x => x.Item1 == 0).ToList();
+
+            var scores = notCorrupted.Select(x => GetScoreByStack(x.Item2)).Where(x => x > 0).OrderBy(x => x).ToList();
+            var toGet = (scores.Count() - 1) / 2;
+
+            foreach (var s in scores)
+            {
+                Console.WriteLine($"{s}");
+            }
+
+            var result = scores[toGet];
+
+            Console.WriteLine($"Solve2 result: {result}");
+        }
+
+
+        static (int, Stack<char>) GetFirstIllegalChar(string input)
+        {
+            var stek = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '('
+                 || input[i] == '['
+                 || input[i] == '{'
+                 || input[i] == '<')
+                {
+                    stek.Push(input[i]);
+                    continue;
+                }
+
+                if (input[i] == ']')
+                {
+                    var top = stek.Count > 0 ? stek.Pop() : '0';
+                    if (top != '[')
+                    {
+                        return (57, stek);
+                    }
+                }
+
+                if (input[i] == ')')
+                {
+                    var top = stek.Count > 0 ? stek.Pop() : '0';
+                    if (top != '(')
+                    {
+                        return (3, stek);
+        }
+                }
+
+                if (input[i] == '}')
+                {
+                    var top = stek.Count > 0 ? stek.Pop() : '0';
+                    if (top != '{')
+                    {
+                        return (1197, stek);
+                    }
+                }
+
+                if (input[i] == '>')
+                {
+                    var top = stek.Count > 0 ? stek.Pop() : '0';
+                    if (top != '<')
+                    {
+                        return (25137, stek);
+    }
+                }
+            }
+
+            return (0, stek);
+        }
+
+        static long GetScoreByStack(Stack<char> stek)
+        {
+            long score = 0;
+
+            while (stek.Count > 0)
+            {
+                var current = stek.Pop();
+                score *= 5;
+                if (current == '(')
+                { 
+                    score += 1;
+                }
+                if (current == '[')
+                {
+                    score += 2;
+                }
+                if (current == '{')
+                {
+                    score += 3;
+                }
+                if (current == '<')
+                {
+                    score += 4;
+                }
+            }
+
+            return score;
+        }
+    }
+}
---
+++ Day11/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solve1(InputData.GetInput(), 100);
+            Solve2(InputData.GetInput());
+        }
+
+        static void Solve2(string[] input)
+        {
+            var lista = new List<int>();
+            foreach (var i in input)
+            {
+                lista.AddRange(i.ToCharArray().Select(x => x - '0'));
+            }
+
+            var convertedInput = lista.ToArray();
+            DisplayMatrix(convertedInput, 10);
+            
+            
+            for (int i = 1;; i++)
+            {
+                var flashes = DoOneStep(convertedInput);
+                if (flashes == convertedInput.Length)
+                {
+                    Console.WriteLine($"First big flash is at step {i}");
+                    break;
+                }
+            }
+
+            
+        }
+
+        static void Solve1(string[] input, int stepsToSimulate)
+        {
+            var lista = new List<int>();
+            foreach (var i in input)
+            {
+                lista.AddRange(i.ToCharArray().Select(x => x - '0'));
+            }
+
+            var convertedInput = lista.ToArray();
+            DisplayMatrix(convertedInput, 10);
+            int steps = stepsToSimulate;
+            long flashes = 0;
+            for (int i = 0; i < steps; i++)
+            {
+                flashes += DoOneStep(convertedInput);
+                DisplayMatrix(convertedInput, 10);
+            }
+
+            Console.WriteLine($"First result is {flashes}");
+        }
+
+        private static void DisplayMatrix(int[] convertedInput, int baza)
+        {
+            for (int i = 0; i < baza; i++)
+            {
+                for (int j = 0; j < baza; j++)
+                {
+                    Console.Write(convertedInput[baza * i + j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(" =============== ");
+        }
+
+        static long DoOneStep(int[] input)
+        {
+            bool[] flashed = new bool[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                ++input[i];
+            }
+
+            bool hadChange = false;
+            var counter = 0;
+            do
+            {
+                hadChange = false;
+                counter++;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (!flashed[i] && input[i] > 9)
+                    {
+                        flashed[i] = true;
+                        hadChange = true;
+                        var surr = GetSurrounding(i, 10);
+                        foreach (var j in surr)
+                        {
+                            input[j]++;
+                        }
+                    }
+                }
+            } while (hadChange && counter < 100);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (flashed[i]) 
+                {
+                    input[i] = 0;
+                }
+            }
+
+            return flashed.Count(x => x);
+        }
+
+        static int[] GetSurrounding(int pos, int baza)
+        {
+            var results = new List<int>();
+
+            if (pos % baza != 0)
+            {
+                results.Add(pos - baza - 1);
+                results.Add(pos + baza - 1);
+                results.Add(pos - 1);
+            }
+
+            if (pos % baza != (baza - 1))
+            {
+                results.Add(pos - baza + 1);
+                results.Add(pos + baza + 1);
+                results.Add(pos + 1);
+            }
+            
+            results.Add(pos - baza);
+           
+            
+            results.Add(pos + baza);
+           
+            
+
+            return results.Where(x => x >= 0 && x < 100).ToArray();
+        }
+    }
+}
