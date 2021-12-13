8ae2e791b34d64928263c98e4df8df4d6f0e7d9b
Zlatko Radojcic <zlatko.rad.alt@gmail.com>
2021-12-13 06:47:06 +0100

    Day13 solution

---
+++ Day13/Point.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13
+{
+    public struct Point : IEqualityComparer<Point>, IEquatable<Point>
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(string input) : this()
+        {
+            var data = input.Split(",");
+            X = Convert.ToInt32(data[0], 10);
+            Y = Convert.ToInt32(data[1], 10);
+        }
+
+        public Point(int x, int y) : this()
+        {
+            X = x;
+            Y = y;
+        }
+
+        public bool Equals(Point other)
+        {
+            if (X != other.X)
+                return false;
+
+            return Y == other.Y;
+        }
+
+        public static bool operator ==(Point point1, Point point2)
+        {
+            return point1.Equals(point2);
+        }
+
+        public static bool operator !=(Point point1, Point point2)
+        {
+            return !point1.Equals(point2);
+        }
+
+        public int GetHashCode(Point obj)
+        {
+            return (obj.X * 10000 + obj.Y).GetHashCode();
+        }
+
+        public bool Equals(Point x, Point y)
+        {
+            return x.Equals(y);
+        }
+    }
+
+}
---
+++ Day13/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solve(InputData.GetInput());
+        }
+
+        static void Solve(string[] input)
+        {
+            var startingPoints = new List<Point>();
+            var folds = new List<(char, int)>();
+
+            foreach (var i in input)
+            {
+                if (string.IsNullOrEmpty(i))
+                {
+                    continue;
+                }
+
+                if (Char.IsNumber(i[0]))
+                {
+                    startingPoints.Add(new Point(i));
+                    continue;
+                }
+
+                var split = i.Split(" ");
+                var split2 = split[2].Split("=");
+                folds.Add((split2[0][0], int.Parse(split2[1])));
+            }
+
+            var result1 = DoFold(startingPoints, folds.First());
+
+            Console.WriteLine($"Points after first fold: {result1.Count}");
+
+            var result2 = new List<Point>();
+            result2.AddRange(startingPoints);
+            foreach (var fold in folds)
+            {
+                result2 = DoFold(result2, fold);
+            }
+
+            var maxX = result2.Aggregate(0, (x, i) => x > i.X ? x : i.X) +1;
+            var maxY = result2.Aggregate(0, (y, i) => y > i.Y ? y : i.Y) +1;
+            Console.WriteLine($"Points after all folds: {result2.Count}");
+            bool[,] drawing = new bool[maxX+1, maxY];
+            for (int i = 0; i < result2.Count; i++)
+            {
+                drawing[result2[i].X,result2[i].Y] = true;
+            }
+
+
+           // Solution for day 2 needs to be visually detected in console.
+            for (int i = 0; i < maxY; i++)
+            {
+                for (int j = 0; j < maxX; j++)
+                {
+                    Console.Write(drawing[j, i] ? "#" : ".");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        static List<Point> DoFold(List<Point> Input, (char, int) foldPosition)
+        {
+            if (foldPosition.Item1 == 'x')
+            {
+                return DoXFold(Input, foldPosition.Item2);
+            }
+            else if (foldPosition.Item1 == 'y')
+            {
+                return DoYFold(Input, foldPosition.Item2);
+            }
+
+            return new List<Point>();
+        }
+
+        static List<Point> DoXFold(List<Point> input, int foldPos)
+        {
+            HashSet<Point> result = new HashSet<Point>();
+            foreach (var i in input)
+            {
+                if (i.X <= foldPos)
+                {
+                    result.Add(i);
+                    continue;
+                }
+
+                var diff = i.X - foldPos;
+                result.Add(new Point(foldPos - diff, i.Y));
+            }
+
+            return result.ToList();
+        }
+
+        static List<Point> DoYFold(List<Point> input, int foldPos)
+        {
+            HashSet<Point> result = new HashSet<Point>();
+            foreach (var i in input)
+            {
+                if (i.Y <= foldPos)
+                {
+                    result.Add(i);
+                    continue;
+                }
+
+                var diff = i.Y - foldPos;
+                result.Add(new Point(i.X, foldPos - diff));
+            }
+
+            return result.ToList();
+        }
+    }
+}
