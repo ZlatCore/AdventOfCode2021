fa9d759c8bab31b7e6a7c6f5256c005c213c0030
Zlatko Radojcic <zlatko.rad.alt@gmail.com>
2021-12-17 12:50:42 +0100

    Day17 part 2. Part 1 i did by hand

---
+++ Day17/InputData.cs
@@ -0,0 +1,23 @@
+﻿namespace Day15
+{
+    public class InputData
+    {
+
+        public static string[] GetTestInput()
+        {
+            return new string[]
+                {
+                  "target area: x=20..30, y=-10..-5"
+                };
+        }
+
+
+        public static string[] GetInput()
+        {
+            return new string[]
+                {
+                    "target area: x=143..177, y=-106..-71"
+                };
+        }
+    }
+}
---
+++ Day17/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Day17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[4] { 143, 177, -106, -71 };
+            Solve2(input);
+        }
+
+        static void Solve2(int[] input)
+        {
+            var count = 0;
+
+            for (int x = 1; x < 178; x++)
+            {
+                
+                for (int y = -106; y < 106; y++)
+                {
+                    count += CheckVector(input, x, y) ? 1 : 0;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+
+        static bool CheckVector(int[] target, int x, int y)
+        {
+            var velx = x;
+            var vely = y;
+            var currentx = 0;
+            var currenty = 0;
+            for (int i = 0; i < 999; i++)
+            {
+                currentx += velx;
+                currenty += vely;
+                velx = Math.Max(0, velx - 1);
+                vely--;
+
+                if (currentx >= target[0] && currentx <= target[1]
+                 && currenty >= target[2] && currenty <= target[3])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
